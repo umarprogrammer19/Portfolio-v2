946d33f6e877ef6b52c748dac7328ed263e17c2b
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-09 00:27:20 +0500

    Changes In Education

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,36 +2,25 @@ import { type ExperienceShowcaseListItemProps } from "@/components/experience/ex
 
 export const EDUCATION: ExperienceShowcaseListItemProps[] = [
   {
-    title: "Bachelor Of Engineering in Electronics and Telecommunication",
+    title: "Intermediate In Pre-Engineering",
     organisation: {
-      name: "Rizvi College Of Engineering",
-      href: "https://eng.rizvi.edu.in/",
+      name: "Adamjee Government Science College",
+      href: "",
     },
-    date: "2019-2023",
-    location: "Bandra, Mumbai",
+    date: "Present",
+    location: "Pakistan, Karachi",
     description:
-      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+      "Currently Doing Inter In Pre-Engineering From Adamjee Government Science College.",
   },
   {
-    title: "Higher Secondary School",
+    title: "Matriculation",
     organisation: {
-      name: "Mithibai College",
-      href: "https://www.mithibai.ac.in/",
+      name: "Hermain Acme Grammer School",
+      href: "",
     },
-    date: "2016-2018",
-    location: "Vile Parle, Mumbai",
+    date: "2021-2023",
+    location: "Pakistan, Karachi",
     description:
-      "Completed the course in Electronics Bifocal Science with 68%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
-  },
-  {
-    title: "Secondary School Education",
-    organisation: {
-      name: "Holy Family High School",
-      href: "https://holyfamilyandheri.org/",
-    },
-    date: "2017-2018",
-    location: "Andheri, Mumbai",
-    description:
-      "Learned good values,dedication and disciplne. Played district football for 5 years.",
+      "Completed Matriculation From Hermain Acme Grammer School.",
   },
 ];
