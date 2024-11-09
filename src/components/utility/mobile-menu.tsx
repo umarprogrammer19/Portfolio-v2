18eba5eb0432a49d6049c6ed4fe4772542d616e2
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-09 15:28:29 +0500

    Improved Navbar

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -58,7 +58,7 @@ export default function MobileMenu({
                 ))}
                 <ThemeSwitch setClose={setOpenMenu} />
               </div>
-              <div className="absolute bottom-0 py-6">©2023 Amit Chauhan</div>
+              <div className="absolute bottom-0 py-6">©2024 Umar Farooq</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
